ece0dddccea7cb67749f6111bb0790df64632b44
darker <darkerro@yandex.ru>
2022-11-24 01:08:01 +0300

    feat: add base styles

---
+++ client/src/Components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LastUpdates from "../LastUpdates/LastUpdates";
+
+function App() {
+  return (
+    <div className="App">
+        <LastUpdates/>
+    </div>
+  );
+}
+
+export default App;
