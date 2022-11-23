d14588464dc052b339b0b790e350568b1e04bb6a
darker <darkerro@yandex.ru>
2022-11-24 01:08:51 +0300

    feat: add title

---
+++ client/src/Components/LastUpdates/LastUpdates.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './LastUpdates.module.css'
+import Title from "../Title/Title";
+
+const LastUpdates = () => {
+    return (
+        <section className={styles.LastUpdates}>
+            <Title>Последнии обновления</Title>
+        </section>
+    );
+};
+
+export default LastUpdates;
